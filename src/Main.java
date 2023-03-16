d7d162bf7bdf8cbba4c7488a081b8127eac51de0
[RoomanHass] <[hasan0v.rom@yandex.ru]>
2023-03-16 19:31:24 +0300

    Внес правки

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) {
         List<Person> human = new ArrayList<Person>();
-        human.add(new Person("John", "Silver", 23));
+        human.add(new Person("John", "Silver Junior", 23));
         human.add(new Person("Glad", "Sokoban", 36));
         human.add(new Person("Stan", "Li", 45));
         human.add(new Person("Vera", "Dark", 20));
